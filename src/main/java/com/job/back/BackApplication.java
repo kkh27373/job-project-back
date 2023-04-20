42e6dd9aefd36511d398b7429d70e3303f78c9c9
Kim <rlagusxo96652@naver.com>
2023-04-20 15:28:14 +0900

    병합 연습

--- src/main/java/com/job/back/BackApplication.java
+++ src/main/java/com/job/back/BackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackApplication {
 	// 주석
 	// 가나?
+	// main으롭 병합 연습 
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
