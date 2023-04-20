ca6655c9ebe4506cae3dfb1d90e093355f9c67e0
dalyume2876 <bold20030@gmail.com>
2023-04-20 15:17:07 +0900

    202

--- src/main/java/com/job/back/BackApplication.java
+++ src/main/java/com/job/back/BackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackApplication {
-	// 주석
+	// asdasd
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
