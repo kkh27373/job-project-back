70532eb21171de6d0fb9c7b737cd32604ec54a67
dalyume2876 <bold20030@gmail.com>
2023-04-20 19:01:00 +0900

    0420-2

--- src/main/java/com/job/back/entity/CompanySelectComponentEntity.java
+++ src/main/java/com/job/back/entity/CompanySelectComponentEntity.java
@@ -28,20 +28,87 @@ public class CompanySelectComponentEntity {
 
     
     @JsonProperty("서울대학교")
-    public int gradeOne() {
+    public int seoulUniversity() {
         return userFinalEducationGradeOne;
     }
 
     @JsonProperty("연세대학교")
-    public int gradeTwo() {
+    public int yonseiUniversity() {
         return userFinalEducationGradeOne;
     }
 
     @JsonProperty("고려대학교")
-    public int gradeThree() {   
+    public int koreaUniversity() {   
         return userFinalEducationGradeOne;
     }
 
-    
+    //=============================================
+
+    @JsonProperty("울산대학교")
+    public int ulsanUniversity() {   
+        return userFinalEducationGradeTwo;
+    }
+
+    @JsonProperty("중앙대학교")
+    public int chungAngUniversity() {   
+        return userFinalEducationGradeTwo;
+    }
+
+    @JsonProperty("부산대학교")
+    public int busanUniversity() {   
+        return userFinalEducationGradeTwo;
+    }
+
+    @JsonProperty("아주대학교")
+    public int jijuUniversity() {   
+        return userFinalEducationGradeTwo;
+    }
+
+    @JsonProperty("서강대학교")
+    public int sogangUniversity() {   
+        return userFinalEducationGradeTwo;
+    }
+
+    //=============================================
+
+    @JsonProperty("전북대학교")
+    public int chonbukUniversity() {   
+        return userFinalEducationGradeThree;
+    }
+
+    @JsonProperty("영남대학교")
+    public int yeungnamUniversity() {   
+        return userFinalEducationGradeThree;
+    }
+
+    @JsonProperty("충남대학교")
+    public int chungnamUniversity() {   
+        return userFinalEducationGradeThree;
+    }
+
+    @JsonProperty("강원대학교")
+    public int kangwonUniversity() {   
+        return userFinalEducationGradeThree;
+    }
+
+    @JsonProperty("한림대학교")
+    public int hallymUniversity() {   
+        return userFinalEducationGradeThree;
+    }
+
+    @JsonProperty("기타")
+    public int anotherUniversity() {   
+        return userFinalEducationGradeEtc;
+    }
+
+    @JsonProperty("경력")
+    public int career(){
+        return userCareerGradeOne;
+    }
+
+    @JsonProperty("자격증")
+    public int lisence(){
+        return userLiceseGradeOne;
+    }
     
 }
