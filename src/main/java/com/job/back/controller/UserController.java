c1eebfe6894d80bcbb1e2e23666116be8b40df35
kkh27373 <kkh27373@naver.com>
2023-04-24 17:34:51 +0900

    05.34

--- src/main/java/com/job/back/controller/UserController.java
+++ src/main/java/com/job/back/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.job.back.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.job.back.service.UserService;
+
+@RestController
 public class UserController {
     
+    // @Autowired private UserService userService;
 }
--- src/main/java/com/job/back/service/UserService.java
+++ src/main/java/com/job/back/service/UserService.java
@@ -1,5 +1,11 @@
 package com.job.back.service;
 
+import com.job.back.dto.request.user.PatchUserComponentDto;
+import com.job.back.dto.responser.ResponseDto;
+import com.job.back.dto.responser.user.GetUserResponseDto;
+import com.job.back.dto.responser.user.PatchUserComponentResponseDto;
+
 public class UserService {
-    
+    public ResponseDto<PatchUserComponentResponseDto> patchProfile(String userFinalEducation, PatchUserComponentDto dto );
+    public ResponseDto<GetUserResponseDto> getUser(String userEmail);
 }
