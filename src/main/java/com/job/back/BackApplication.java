5a27311e172e4d9459c5cf386af8c0866d08cb02
Kim <rlagusxo96652@naver.com>
2023-04-24 14:10:38 +0900

    저장

--- src/main/java/com/job/back/BackApplication.java
+++ src/main/java/com/job/back/BackApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackApplication {
-	// 주석
-	// 가나?
-	// main으롭 git병합 연습 \
-	// asdasd\
-	// 추가작업
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
