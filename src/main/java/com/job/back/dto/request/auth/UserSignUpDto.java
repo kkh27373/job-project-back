2cd42eba68cb62b34c2ad899d873acfac19f16d8
Kim <rlagusxo96652@naver.com>
2023-04-26 15:13:38 +0900

    4월-26일 회원가입까지 완료

--- src/main/java/com/job/back/dto/request/auth/UserSignUpDto.java
+++ src/main/java/com/job/back/dto/request/auth/UserSignUpDto.java
@@ -2,6 +2,7 @@ package com.job.back.dto.request.auth;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -26,7 +27,7 @@ public class UserSignUpDto {
     private String userProfileUrl;
 
     @NotBlank
-    @Length(min=11, max=13)
+    @Length(min=11, max=15)
     private String userTelNumber;
 
     @NotBlank
@@ -35,8 +36,10 @@ public class UserSignUpDto {
     @NotBlank
     private String userAddress;
 
-    @NotBlank
-    private int userAge;
+    // ! int 는 NotBlank 를 사용하지 못한다 
+    // ! ==> int는 null 도 사용 불가 ==> Integer 사용 ==> Not Null을 사용해준다 
+    @NotNull
+    private Integer userAge;
 
     @NotBlank
     private String userGender;
