e331bfe131bdabccc309e24deb39e34604c02894
kkh27373 <kkh27373@naver.com>
2023-04-27 14:13:49 +0900

    0427

--- src/main/java/com/job/back/dto/request/auth/UserSignInDto.java
+++ src/main/java/com/job/back/dto/request/auth/UserSignInDto.java
@@ -13,11 +13,7 @@ import lombok.NoArgsConstructor;
 public class UserSignInDto {
     @NotBlank
     @Email
-<<<<<<< HEAD
-    @Length(max = 45)
-=======
     @Length(max = 30)
->>>>>>> a8c9f25f7e199a62a896193ced62aa0686ea6dd9
     private String userEmail;
     @NotBlank
     @Length(min = 8, max = 50)
