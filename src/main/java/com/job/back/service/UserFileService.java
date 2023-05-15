328900552f6e12f9b292a0b095026be941ea819e
kkh27373 <kkh27373@naver.com>
2023-05-15 16:40:28 +0900

    05.15fileuplod중

--- src/main/java/com/job/back/service/UserFileService.java
+++ src/main/java/com/job/back/service/UserFileService.java
@@ -1,10 +1,10 @@
-// package com.job.back.service;
+package com.job.back.service;
 
-// import org.springframework.core.io.Resource;
-// import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 
-// public interface UserFileService {
-//     public String userUplad(MultipartFile userfile);
-//     public Resource getUserFile(String fileName);
+public interface UserFileService {
+    public String userUpload(MultipartFile userfile);
+    public Resource getUserFile(String fileName);
 
-// }
+}
--- src/main/java/com/job/back/service/implementation/UserFileServiceImplementation.java
+++ src/main/java/com/job/back/service/implementation/UserFileServiceImplementation.java
@@ -1,62 +1,62 @@
-// package com.job.back.service.implementation;
+package com.job.back.service.implementation;
 
-// import java.io.File;
-// import java.util.UUID;
+import java.io.File;
+import java.util.UUID;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.core.io.Resource;
-// import org.springframework.core.io.UrlResource;
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-// import com.job.back.service.UserFileService;
+import com.job.back.service.UserFileService;
 
-// @Service
-// public class UserFileServiceImplementation implements UserFileService {
+@Service
+public class UserFileServiceImplementation implements UserFileService {
 
-//    @Value("${user-file.path}")
-//    private String USER_FILE_PATH;
-//    @Value("${user-file.url}")
-//    private String USER_FILE_URL;
+   @Value("${file.path}")
+   private String USER_FILE_PATH;
+   @Value("${file.url}")
+   private String USER_FILE_URL;
 
 
-//    public String userUplad(MultipartFile file) {
+   public String userUpload(MultipartFile file) {
 
-//         if(file.isEmpty()) return null;
+        if(file.isEmpty()) return null;
 
-//         String originalFileName = file.getOriginalFilename();
-//         String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String originalFileName = file.getOriginalFilename();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
 
-//         String uuid = UUID.randomUUID().toString();
+        String uuid = UUID.randomUUID().toString();
 
-//         String saveName = uuid + extension;
-//         String savePath = USER_FILE_PATH + saveName;
+        String saveName = uuid + extension;
+        String savePath = USER_FILE_PATH + saveName;
 
-//         try {
-//             file.transferTo(new File(savePath));
-//         } catch (Exception exception){
-//             exception.printStackTrace();
-//             return null;
-//         }
+        try {
+            file.transferTo(new File(savePath));
+        } catch (Exception exception){
+            exception.printStackTrace();
+            return null;
+        }
 
-//         String url = USER_FILE_PATH + saveName;
-//         return url;
+        String url = USER_FILE_PATH + saveName;
+        return url;
 
-//     }
+    }
 
-//     public Resource getUserFile(String fileName) {
+    public Resource getUserFile(String fileName) {
 
-//         Resource resource = null;
+        Resource resource = null;
 
-//         try{
-//             resource = new UrlResource("file:" + USER_FILE_PATH + fileName);
-//         } catch(Exception exception){
-//             exception.printStackTrace();
-//             return null;
-//         }
+        try{
+            resource = new UrlResource("file:" + USER_FILE_PATH + fileName);
+        } catch(Exception exception){
+            exception.printStackTrace();
+            return null;
+        }
 
-//         return resource;
-//     }
+        return resource;
+    }
 
     
-// }
+}
