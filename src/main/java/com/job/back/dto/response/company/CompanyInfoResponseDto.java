917969eb5d08650f3cbee5cb4328aee18f049067
dalyume2876 <bold20030@gmail.com>
2023-05-25 15:21:06 +0900

    ResponseDto Api info

--- src/main/java/com/job/back/dto/response/company/CompanyInfoResponseDto.java
+++ src/main/java/com/job/back/dto/response/company/CompanyInfoResponseDto.java
@@ -1,12 +1,16 @@
 package com.job.back.dto.response.company;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "회사 정보 response body - data")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanyInfoResponseDto {
+    @ApiModelProperty(value = "회사 정보 결과", example = "true", required = true)
     private boolean result;
 }
